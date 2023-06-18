b02e8a6bda1a46fc47407102c39778f30673a5e5
Dam Podraza <damianpodraza@icloud.com>
2023-06-18 16:21:56 +0200

    add calendar

---
+++ pages/rezerwacja/index.js
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react';
+import Layout from '../../components/ui/layout';
+import Cal, { getCalApi } from '@calcom/embed-react';
+
+const Rezerwacja = () => {
+    useEffect(() => {
+        (async function () {
+          const Cal = await getCalApi();
+          Cal("on", {
+            action: "__windowLoadComplete",
+            callback: (e) => {
+              console.log('load complete');
+            }
+          });
+        })();
+      }, []);
+
+  return (
+    <Layout>
+      <div className='pt-32 pb-16'>
+        <Cal calLink='damian-podraza-f6c5dg'></Cal>
+      </div>
+    </Layout>
+  );
+};
+
+export default Rezerwacja;
