f80ba72461e4000f12ee4aff6a6ac19c86de78a2
Damian Podraza <damianpodraza@icloud.com>
2023-06-21 19:25:45 +0200

    fix hidden desktop front

--- components/FrontSection/frontsection.component.js
+++ components/FrontSection/frontsection.component.js
@@ -25,7 +25,7 @@ const FrontSection = () => {
           </div>
         </div>
       ) : (
-        <div className='relative hidden w-screen px-1'>
+        <div className='relative w-screen px-1'>
           <Image src={landingImgDesktop} alt='kasiaPodrazaLogo' />
           <div className='absolute left-[40%] top-[40%] max-w-[30%] -translate-x-1/2 -translate-y-1/2 transform text-center font-spartan-bold text-base lg:text-xl xl:text-2xl'>
             Nie zawsze możesz kontrolować to, co dzieje się na zewnątrz. <br />
