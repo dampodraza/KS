58a6ae949714dcc2e07ecf479e6462f39216e90e
Damian Podraza <damianpodraza@icloud.com>
2023-07-23 11:08:29 +0200

    add meta

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
 import '../styles/globals.css';
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return 
+  <>
+  <Head>
+        <title>
+          Kasia Podraza | Psychologia, Coaching i joga
+        </title>
+        <meta
+          name="description"
+          content="Pracuję z drugim człowiekiem jako całością w wymiarze fizycznym, psychicznym, emocjonalnym i duchowym. Umów się na konsultację już dziś."
+          key="desc"
+        />
+      </Head>
+  <Component {...pageProps} />;
+  </>
 }
 
 export default MyApp;
